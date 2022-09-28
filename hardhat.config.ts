5a77dfb88cee66859728e9fc381cb892a69a07ee
Amin Hassani <gigilibala4@gmail.com>
2022-09-27 23:13:26 -0700

    [Fixes] Some fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,49 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomiclabs/hardhat-etherscan'
+import 'dotenv/config'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+import '@typechain/hardhat'
+import 'hardhat-deploy'
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || 'https://eth-goerli'
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+    solidity: '0.8.17',
+    defaultNetwork: 'hardhat',
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY!],
+            chainId: 5
+        },
+        localhost: {
+            url: 'http://127.0.0.1:8545/',
+            chainId: 31337
+        }
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
+    },
+    gasReporter: {
+        enabled: true,
+        currency: 'USD',
+        noColors: true,
+        outputFile: 'gas-report.txt',
+        coinmarketcap: COINMARKETCAP_API_KEY
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0
+        },
+        user: {
+            default: 1
+        }
+    }
+}
 
-export default config;
+export default config
